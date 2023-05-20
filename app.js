c4fd28b7d1a6d8132d256e7f9a5ef8c7fa679300
lakshay gupta <lakshaygupta2511@gmail.com>
2023-05-20 23:52:46 +0530

    Inital Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const parallax_el = document.querySelectorAll(".parallax");
+
+let xValue = 0, yValue = 0;
+
+parallax_el.forEach((el) => {
+    el.style.transform = "translate(-50%, -50%)";
+});
+
+window.addEventListener("mousemove", (e) => {
+
+    xValue = e.clientX - window.innerWidth/2;
+    yValue = e.clientY - window.innerHeight/2;
+
+    
+
+    parallax_el.forEach((el) => {
+
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+        let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+        let zValue = e.clientX - parseFloat(getComputedStyle(el).left) * (isInLeft * 0.00);
+        
+
+        console.log(speedx);
+        el.style.transform = `translateX(calc(-50% + ${
+            -xValue * speedx
+        }px)) translateY(calc(-50% + ${
+                yValue * speedy
+            }px)) perspective(2300px)` ;
+    });
+});
